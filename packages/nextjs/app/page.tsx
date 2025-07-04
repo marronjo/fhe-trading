d31fbb89f26f1bf33a4d674e9576996680a93653
Joe Marron <marronjo@tcd.ie>
2025-07-04 17:56:18 +0100

    remove unused sections from main page

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -3,51 +3,11 @@
 import { Slot0Display } from "./PoolStateViewComponent";
 import { SwapComponent } from "./SwapComponent";
 import type { NextPage } from "next";
-import { useAccount } from "wagmi";
-import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
-
   return (
     <>
       <div className="flex items-center flex-col grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold mb-2">CoFHE-ETH</span>
-            <a
-              className="flex justify-center items-center gap-1"
-              href="https://cofhe-docs.fhenix.zone/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <span className="link">Fhenix CoFHE Documentation</span>
-            </a>
-          </h1>
-          <div className="flex justify-center items-center space-x-2 flex-col">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
-          </div>
-
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              FHECounter.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
-        </div>
-
         <div className="grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col md:flex-row">
             <Slot0Display />
