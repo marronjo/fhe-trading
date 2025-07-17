b7e6252f17f0fd24378a8b3a8fa8120ae0c76f69
Joe Marron <marronjo@tcd.ie>
2025-07-17 16:30:02 +0100

    remove test components from main deployment

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { Slot0Display } from "./PoolStateViewComponent";
 import { SwapComponent } from "./SwapComponent";
-import { TokenFaucet } from "./TokenFaucet";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
@@ -13,14 +11,6 @@ const Home: NextPage = () => {
           <div className="flex justify-center items-center gap-12 flex-col md:flex-row">
             <SwapComponent />
           </div>
-        </div>
-        <div className="grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col md:flex-row">
-            <Slot0Display />
-          </div>
-        </div>
-        <div className="flex-1">
-          <TokenFaucet />
         </div>
       </div>
     </>
